1034999f5528ea518c28819a5ce11141a84d9a02
John Lai <lai.john.py@gmail.com>
2023-01-28 11:23:49 -0500

    Add localStorage functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,33 @@
 const input = document.getElementById('js-input-val');
 const form = document.getElementById('js-search-form');
 
+//Global Vars
+
 // Create a input to take user city - if field empty checks
 function grabUserInput(e) {
   //Remove default form submission
   e.preventDefault();
-  console.log(this.classList.contains('#js-input-val'));
+
+  //check for empty input val
+  if (!input.value) {
+    console.log('nothing in input');
+    return;
+  }
+
+  inputValue = input.value;
+  console.log(inputValue);
+  input.value = '';
 }
 
-// add event listener to form to grab input value
-form.addEventListener('submit', grabUserInput);
+// Display local storage to show history of cities names
+function displaySearchHistory() {
+  let storedHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+}
 
-// Local storage to show history of cities names
+//set local storage
+function setLocalStorage() {
+  localStorage.setItem('searchHistory', JSON.stringify(storedHistory));
+}
 
 // Create fetch to get data from open weather api
 
@@ -21,3 +37,10 @@ form.addEventListener('submit', grabUserInput);
 // Grab city name THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
 
 //5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// add event listener to form to grab input value
+form.addEventListener('submit', grabUserInput);
+
+function init() {}
+
+init();
