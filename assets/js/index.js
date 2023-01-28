be18491d01c626196d2f35ce031a5cc6e8ae1d6b
John Lai <lai.john.py@gmail.com>
2023-01-28 11:01:57 -0500

    Pseudo code in js

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// Create a input to take user city - if field empty checks
+
+// add event listener to form to grab input value
+
+// Local storage to show history of cities names
+
+// Create fetch to get data from open weather api
+
+// display first 6 results
+
+// Grab city name THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+
+//5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
